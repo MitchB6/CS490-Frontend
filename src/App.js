b68aa0f4f69385bf9ea62480faff5d2b37f2c1be
Mitchell <mdb4@njit.edu>
2023-09-14 04:12:54 -0400

    Prints out hello world returned from server.py. Only works once

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+   const [data, setData] = useState([{}])
+   useEffect(() =>{
+      fetch("/healthcheck").then(
+         res => res.json()
+      ).then(
+         data => {
+            setData(data)
+            console.log(data)
+         }
+      )
+   }, [])
   return (
-    <div>App</div>
+    <div>
+      {(data.print[1])}
+      {/* <p>{data.results && data.results[0] && data.results[0].output}</p> */}
+      {/* test */}
+    </div>
   )
 }
 
