20c40c13a9a00187765b864f541e06a30ecc0734
Mitchell <mdb4@njit.edu>
2023-09-24 22:37:18 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
+import './styles.css';
 import React, {useState, useEffect} from 'react'
 
 function App() {
-   const [data, setData] = useState("Loading...")
-   useEffect(() =>{
-      fetch("http://localhost:5000/healthcheck", 
-         {method: "get",
-         headers: {
-            "Content-Type": "application/json",
-            // "Access-Control-Allow-Origin": "main--capable-gumption-8655c3.netlify.app"
-         }}
-      )
-         .then(res => {
-         // res => res.json()
-            if (res.status === 200){
-               return res.json();
-            }else{
-               console.log(res)
-               throw new Error(`Recived status code ${res.status}`)
-            }
-         })
-         .then(data => {
-            setData(data.print)
-            // console.log(data.print)
-         }
-      )
-      .catch(error => {
-         console.error("Fetch failed:", error)
-         setData("Error")
-      })
-   }, [])
+  const [data, setData] = useState("Loading...")
+  const [listItems, setListItems] = useState(null)
+  useEffect(() => {
+    fetch("http://localhost:5000/", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        // "Access-Control-Allow-Origin": "main--capable-gumption-8655c3.netlify.app"
+      }
+    }).then(res => {
+      if (res != null) {
+        return res.json();
+      } else { // console.log(res)
+        throw new Error(`Received status code ${res}`)
+      }
+    }).then(data => {
+      setData(data)
+      const items = data.map((rowData) => <li key={rowData[0]}>{rowData}</li>);
+      setListItems(items)
+    }).catch(error => {
+      console.error("Fetch failed:", error)
+      setData("Error")
+    })
+  }, [])
   return (
-    <div>
-      {(data)}
-      {/* <p>{data.results && data.results[0] && data.results[0].output}</p> */}
-      {/* test */}
+    <div> {
+      data === "Error" ? (
+        console.log("An error occurred while fetching the data.")
+      ) : (
+        <ul>{listItems}</ul>
+      )
+    }
+      <a href="/">
+        <h1 className="top">Home</h1>
+      </a>
+      <div className="row">
+        <a href="https://google.com/">
+          <h2 className="subsection">Movies</h2>
+        </a>
+        <a href="https://yahoo.com/">
+          <h2 className="subsection">Customers</h2>
+        </a>
+        <a href="https://bing.com/">
+          <h2 className="subsection">Reports</h2>
+        </a>
+      </div>
+
     </div>
   )
 }
