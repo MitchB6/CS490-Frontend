1abf75e8de191fd76e7b5ce57e6de5d4b1efdc0b
Mitchell <mdb4@njit.edu>
2023-09-26 16:36:53 -0400

    both list working with printing descripion

--- src/App.js
+++ src/App.js
@@ -2,55 +2,92 @@ import './styles.css';
 import React, {useState, useEffect} from 'react'
 
 function App() {
-  const [data, setData] = useState("Loading...")
-  const [listItems, setListItems] = useState(null)
+  const [movies, setMovies] = useState([]);
+  const [actors, setActors] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("http://localhost:5000/", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        // "Access-Control-Allow-Origin": "main--capable-gumption-8655c3.netlify.app"
-      }
-    }).then(res => {
-      if (res != null) {
-        return res.json();
-      } else { // console.log(res)
-        throw new Error(`Received status code ${res}`)
-      }
-    }).then(data => {
-      setData(data)
-      const items = data.map((rowData) => <li key={rowData[0]}>{rowData}</li>);
-      setListItems(items)
-    }).catch(error => {
-      console.error("Fetch failed:", error)
-      setData("Error")
-    })
-  }, [])
-  return (
-    <div> {
-      data === "Error" ? (
-        console.log("An error occurred while fetching the data.")
-      ) : (
-        <ul>{listItems}</ul>
-      )
-    }
-      <a href="/">
-        <h1 className="top">Home</h1>
-      </a>
-      <div className="row">
-        <a href="https://google.com/">
-          <h2 className="subsection">Movies</h2>
+    Promise.all([
+      fetch("http://localhost:5000/movies", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      fetch("http://localhost:5000/actors", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    ])
+      .then(([moviesRes, actorsRes]) => {
+        if (moviesRes != null && actorsRes != null) {
+          return Promise.all([moviesRes.json(), actorsRes.json()]);
+        } else {
+          throw new Error(`Received status code ${moviesRes.status} and ${actorsRes.status}`);
+        }
+      })
+      .then(([moviesData, actorsData]) => {
+        setMovies(moviesData);
+        setActors(actorsData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Fetch failed:", error);
+        setIsLoading(false);
+      });
+  }, []);
+  
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const movieClickHandler = (movieID) => {
+    const movie = movies.find((movie) => movie[0] === movieID);
+    setSelectedMovie(movie[3]);
+  }
+
+    return (
+      <div>
+        <a href="/">
+          <h1 className="top">Home</h1>
         </a>
-        <a href="https://yahoo.com/">
-          <h2 className="subsection">Customers</h2>
-        </a>
-        <a href="https://bing.com/">
-          <h2 className="subsection">Reports</h2>
-        </a>
+        <div className="row">
+          <a href="https://google.com/">
+            <h2 className="subsection">Movies</h2>
+          </a>
+          <a href="https://yahoo.com/">
+            <h2 className="subsection">Customers</h2>
+          </a>
+          <a href="https://bing.com/">
+            <h2 className="subsection">Reports</h2>
+          </a>
+        </div>
+        <div>
+          <h3>Top 5 Movies</h3>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <li key={movie[0]} onClick={() => movieClickHandler(movie[0])}>{movie[1]}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div>
+          <h3>Top 5 Actors</h3>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {actors.map((actor) => (
+                <li key={actor[0]}>{actor[1]} {actor[2]}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {selectedMovie && <p>{selectedMovie}</p>}
       </div>
+    );
+  }
 
-    </div>
-  )
-}
-
-export default App
+  export default App
