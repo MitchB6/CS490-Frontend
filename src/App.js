8ce91d53707a4214c3f6ef632a17b248ea1981e6
Mitchell <mdb4@njit.edu>
2023-09-26 22:53:30 -0400

    structure with buttons

--- src/App.js
+++ src/App.js
@@ -1,94 +1,52 @@
-import './styles.css';
-import React, {useState, useEffect} from 'react'
+import { useState } from 'react';
+import Main from './Main';
+import Customer from './Customer';
+import Movies from './Movies';
+import Reports from './Reports';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [actors, setActors] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    Promise.all([
-      fetch("http://localhost:5000/movies", {
-        method: "get",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }),
-      fetch("http://localhost:5000/actors", {
-        method: "get",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }),
-    ])
-      .then(([moviesRes, actorsRes]) => {
-        if (moviesRes != null && actorsRes != null) {
-          return Promise.all([moviesRes.json(), actorsRes.json()]);
-        } else {
-          throw new Error(`Received status code ${moviesRes.status} and ${actorsRes.status}`);
-        }
-      })
-      .then(([moviesData, actorsData]) => {
-        setMovies(moviesData);
-        setActors(actorsData);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Fetch failed:", error);
-        setIsLoading(false);
-      });
-  }, []);
-  
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [showMain, setShowMain] = useState(true);
+  const [showMovies, setShowMovies] = useState(false);
+  const [showCustomer, setShowCustomer] = useState(false);
+  const [showReports, setShowReports] = useState(false);
 
-  const movieClickHandler = (movieID) => {
-    const movie = movies.find((movie) => movie[0] === movieID);
-    setSelectedMovie(movie[3]);
+  const mainClickHandler = () => {
+    setShowMain(true);
+    setShowMovies(false);
+    setShowCustomer(false);
+    setShowReports(false);
+  }
+  const moviesClickHandler = () => {
+    setShowMain(false);
+    setShowMovies(true);
+    setShowCustomer(false);
+    setShowReports(false);
+  }
+  const customerClickHandler = () => {
+    setShowMain(false);
+    setShowMovies(false);
+    setShowCustomer(true);
+    setShowReports(false);
+  }
+  const reportsClickHandler = () => {
+    setShowMain(false);
+    setShowMovies(false);
+    setShowCustomer(false);
+    setShowReports(true);
   }
 
-    return (
-      <div>
-        <a href="/">
-          <h1 className="top">Home</h1>
-        </a>
-        <div className="row">
-          <a href="https://google.com/">
-            <h2 className="subsection">Movies</h2>
-          </a>
-          <a href="https://yahoo.com/">
-            <h2 className="subsection">Customers</h2>
-          </a>
-          <a href="https://bing.com/">
-            <h2 className="subsection">Reports</h2>
-          </a>
-        </div>
-        <div>
-          <h3>Top 5 Movies</h3>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <ul>
-              {movies.map((movie) => (
-                <li key={movie[0]} onClick={() => movieClickHandler(movie[0])}>{movie[1]}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div>
-          <h3>Top 5 Actors</h3>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <ul>
-              {actors.map((actor) => (
-                <li key={actor[0]}>{actor[1]} {actor[2]}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-        {selectedMovie && <p>{selectedMovie}</p>}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p><button onClick={mainClickHandler}>Home</button></p>
+      <button onClick={moviesClickHandler}>Movies</button>
+      <button onClick={customerClickHandler}>Customer</button>
+      <button onClick={reportsClickHandler}>Reports</button>
+      {showMain && <Main />}
+      {showMovies && <Movies />}
+      {showCustomer && <Customer />}
+      {showReports && <Reports />}
+    </div>
+  );
+}
 
-  test
   export default App
